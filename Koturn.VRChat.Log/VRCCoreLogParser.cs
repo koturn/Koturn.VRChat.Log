14fa603f7c464ba712587470e2c7839bea6924b5
koturn <jeak.koutan.apple@gmail.com>
2025-05-10 12:34:57 +0900

    chore: Remove unnecessary "using"

--- Koturn.VRChat.Log/VRCCoreLogParser.cs
+++ Koturn.VRChat.Log/VRCCoreLogParser.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Koturn.VRChat.Log.Enums;
 using Koturn.VRChat.Log.Exceptions;
 using Koturn.VRChat.Log.Internals;
